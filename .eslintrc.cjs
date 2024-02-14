4979b813f969b5b058da50d0dbb771769d9392c6
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2024-02-14 18:29:10 +0900

    build: Tdd jest 관련 라이브러리 설치

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,11 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, module: 'node' },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'react-app/jest',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
